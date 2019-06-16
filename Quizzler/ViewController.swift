88b80505b39f0f481f8c0c9c311da524a2753ec5
Ahmed Aboelela <ahmed@Ahmeds-Mac.local>
2019-06-16 13:34:42 -0700

    Quizler final

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    let questions = QuestionBank()
+    var questionIndex : Int = 0
+    var score : Int = 0
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        questionLabel.text = questions.list[questionIndex].questionText
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            checkAnswer(userAnswer: true)
+        }else{
+            checkAnswer(userAnswer: false)
+        }
+        updateUI()
+    }
+    
+    
+    func updateUI() {
+        progressLabel.text = "\(questionIndex+1)/13"
+        scoreLabel.text = "Score: \(score)"
+        progressBar.frame.size.width = (view.frame.size.width/13) * CGFloat(questionIndex+1)
+    }
+    
+
+    func nextQuestion() {
+        questionIndex += 1
+        dispQuestion(questionIndex: questionIndex)
+        
+    }
+    
+    
+    func checkAnswer(userAnswer: Bool) {
+        if(questions.list[questionIndex].answer == userAnswer){
+            showAlert(resultMessage: "You got it!")
+            score+=1
+        }else{
+            showAlert(resultMessage: "Wrong Answer")
+        }
+        if questionIndex == questions.list.count-1 	{
+            startOver()
+        }else{
+            nextQuestion()
+        }
+    }
+    
+    
+    func startOver() {
+       questionIndex = 0
+        dispQuestion(questionIndex: questionIndex)
+    }
+    
+    func showAlert(resultMessage : String){
+        let alertController = UIAlertController(title: "Result", message: resultMessage, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "ok", style: .default) { (UIAlertAction) in
+            alertController.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func dispQuestion(questionIndex: Int){
+        questionLabel.text = questions.list[questionIndex].questionText
+    }
+    
+}
